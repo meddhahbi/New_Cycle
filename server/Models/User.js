3a81ad7dcb5d7651d0fdad6e0bcc8b5b15224b79
Mohamed Dhahbi <56275931+med850@users.noreply.github.com>
2023-03-04 10:59:41 +0100

    Login method & make a test with postman

--- server/Models/User.js
+++ server/Models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 
 
@@ -21,6 +22,12 @@ var url = 'mongodb://localhost:27018/newCycle';
 
 var User = mongoose.model('User',schemaUser);
 //var url = process.env.URL;
+
+var privateKey = "this is my secret key testjsdjsbdjdbdjbcjbkajdbqsjq"
+
+
+
+
 
 exports.register=(username,email,password,phone,postal,role)=>{
     return new Promise((resolve,reject)=>{
@@ -61,4 +68,43 @@ exports.register=(username,email,password,phone,postal,role)=>{
     })
 }
 
+exports.login=(email,password)=>{
+    return new Promise((resolve, reject)=>{
+        mongoose.connect(url,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        }).then(()=>{
+            return User.findOne({ email:email})
+        }).then((user)=>{
+            if(!user){
+                mongoose.disconnect();
+                reject("this email does not exist");
+            }else{
+                bcrypt.compare(password, user.password).then((same)=>{
+                    //console.log("same password");
+                        if(same){
+                            //?send token
+                            let token = jwt.sign({
+                                id:user._id,
+                                username:user.username
+                            },privateKey,{
+                                expiresIn:'1h',
+                            })
+                            mongoose.disconnect();
+                            resolve(token);
+                            jwt.decode();
+
+
+                        }else{
+                            mongoose.disconnect();
+                            reject('invalid password')
+                        }
+                }).catch((err)=>{
+                    mongoose.disconnect();
+                    reject(err);
+                })
+            }
+        })
+    })
+}
 
