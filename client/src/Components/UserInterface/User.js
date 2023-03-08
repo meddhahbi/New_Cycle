14dbffe44150a1c8c79e76e674d1e6368d5b17dd
Mohamed Dhahbi <56275931+med850@users.noreply.github.com>
2023-03-08 18:44:15 +0100

    Template integration

---
+++ client/src/Components/UserInterface/User.js
@@ -0,0 +1,22 @@
+import { Outlet } from "react-router"
+import Navbar from "../Navbar"
+import Footer from "./Footer"
+
+
+export default function User(){
+
+
+    return <div>
+
+        <div><Navbar /></div>
+        <Outlet />
+        <div><Footer /></div>
+
+
+
+    </div>
+
+
+
+
+}
