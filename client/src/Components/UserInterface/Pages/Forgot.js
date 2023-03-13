69541e69dafb17ef1dbbe132d0fb6b319641f11d
Wassim-Aloui <61364693+Wassim-Aloui@users.noreply.github.com>
2023-03-14 00:28:17 +0100

    add email validation in forgot password

--- client/src/Components/UserInterface/Pages/Forgot.js
+++ client/src/Components/UserInterface/Pages/Forgot.js
@@ -2,24 +2,29 @@ import { useState } from "react";
 import axios from "axios";
 
 
-export default function Forgot(){
+export default function Forgot() {
     const [email, setEmail] = useState("");
-
-
-	const handleSubmit = async (e) => {
-		e.preventDefault();
-		try {
-			const url = `http://localhost:3001/resetPwd`;
-			const { data } = await axios.post(url, { email });
-		} catch (error) {
-			if (
-				error.response &&
-				error.response.status >= 400 &&
-				error.response.status <= 500
-			) ;
-		}
-	};
-    
+    const [errorMessage, setErrorMessage] = useState("");
+  
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      try {
+        const url = `http://localhost:3001/resetPwd`;
+        const { data } = await axios.post(url, { email });
+      } catch (error) {
+        console.log(error);
+        if (
+          error.response &&
+          error.response.status >= 400 &&
+          error.response.status <= 500
+        ) {
+          setErrorMessage("This email does not exist");
+        }
+        setTimeout(() => {
+            setErrorMessage("");
+          }, 5000); // hide error message after 5 seconds
+      }
+    };
     return <section className="log-in-section section-b-space forgot-section">
     <div className="container-fluid-lg w-100">
         <div className="row">
@@ -43,6 +48,7 @@ export default function Forgot(){
                                     <div className="form-floating theme-form-floating log-in-form">
                                         <input type="email" className="form-control" id="email"
                                             placeholder="Email Address" onChange={(e) => setEmail(e.target.value)} value={email}/>
+                                             {errorMessage && <p>{errorMessage}</p>}
                                         <label htmlFor="email" >Email Address</label>
                                     </div>
                                 </div>
@@ -52,6 +58,7 @@ export default function Forgot(){
                                         Password</button>
                                 </div>
                             </form>
+                           
                         </div>
                     </div>
                 </div>
