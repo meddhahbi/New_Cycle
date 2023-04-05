3023a75970731844efb310eb23882b45932e20f1
Mohamed Dhahbi <56275931+med850@users.noreply.github.com>
2023-04-05 18:50:15 +0200

    Finish with document verification for association

--- client/src/Components/UserInterface/Pages/LoadAssociation.js
+++ client/src/Components/UserInterface/Pages/LoadAssociation.js
@@ -1,11 +1,37 @@
 import { BoltLoader } from "react-awesome-loaders";
-import './style/loader.css'
+import './style/loader.css';
+import React, { useEffect, useState } from 'react';
 
 
-// const timeout = setTimeout(() => {
-//   window.location.href = '/login'; // replace with your desired URL
-// }, 3000);
+
+
 export const LoadAssociation = () => {
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      window.location.href = '/loginAssociation'; 
+    }, 9000); 
+    return () => clearTimeout(timeout);
+  }, []);
+
+
+  const [showFirstMessage, setShowFirstMessage] = useState(true);
+
+  useEffect(() => {
+    const firstTimer = setTimeout(() => {
+      setShowFirstMessage(false);
+    }, 5000);
+
+    const secondTimer = setTimeout(() => {
+      setShowFirstMessage(true);
+    }, 10000);
+
+    return () => {
+      clearTimeout(firstTimer);
+      clearTimeout(secondTimer);
+    };
+  }, []);
+
   return (
     <div className="loader">
    <center>     <BoltLoader
@@ -13,7 +39,12 @@ export const LoadAssociation = () => {
         boltColor={"#3CB371"}
         backgroundBlurColor={"#3CB371"}
       /><br/><br/><br/><br/>
-      <h2>Loading</h2>
+      {/* <h2>Please wait Verification for the document</h2> */}
+      {showFirstMessage ? (
+          <h2>Please wait Verification for the document</h2>
+        ) : (
+          <h2>You can Log In now</h2>
+        )}
       </center>
     </div>
   );
